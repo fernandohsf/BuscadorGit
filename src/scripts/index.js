f16138081e56e9575970182393f3346a910121ea
Fernando Henrique da Silva Ferreira <fernandohsferreira@gmail.com>
2024-01-17 16:06:13 -0400

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,7 @@
-import { getUser } from "/src/scripts/services/users.js"
-import { getRepos } from "/src/scripts/services/repositories.js"
-import { user } from "/src/scripts/objects/user.js"
-import { screen } from "/src/scripts/objects/screen.js"
+import { getUser } from "./services/users.js"
+import { getRepos } from "./services/repositories.js"
+import { user } from "./objects/user.js"
+import { screen } from "./objects/screen.js"
 
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
