7e52a2dc74969a77e34bb69682cbaa22d3b3a27f
Fernando Henrique da Silva Ferreira <fernandohsferreira@gmail.com>
2024-01-17 15:56:10 -0400

    Update repositories.js

--- src/scripts/services/repositories.js
+++ src/scripts/services/repositories.js
@@ -1,4 +1,4 @@
-import { baseUrl, repositoriesQuantity } from "/src/scripts/variables.js"
+import { baseUrl, repositoriesQuantity } from "./src/scripts/variables.js"
 async function getRepos(userName){
     const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesQuantity}`)
     return await response.json()
