50d8937a43dba9d6429d0e1fa67e8c5227f6d8d9
Fernando Henrique da Silva Ferreira <fernandohsferreira@gmail.com>
2024-01-17 15:34:24 -0400

    Projeto finalizado

---
+++ src/scripts/services/repositories.js
@@ -0,0 +1,6 @@
+import { baseUrl, repositoriesQuantity } from "/src/scripts/variables.js"
+async function getRepos(userName){
+    const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesQuantity}`)
+    return await response.json()
+}
+export{getRepos}
