a6546f9f3c45e62e69fdbb57d348ed4a93fb9e9d
Fernando Henrique da Silva Ferreira <fernandohsferreira@gmail.com>
2024-01-18 08:27:26 -0400

    Delete src/scripts/services/repositories.js

--- src/scripts/services/repositories.js
+++
@@ -1,7 +0,0 @@
-import { baseUrl, repositoriesQuantity } from "../variables.js"
-
-async function getRepos(userName){
-    const response = await fetch(`${baseUrl}/${userName}/repos?per_page${repositoriesQuantity}`)
-    return await response.json()
-}
-export {getRepos}
