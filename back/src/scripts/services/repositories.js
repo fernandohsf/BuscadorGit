b20c1968f029dcfe4ef6dfe2197471b7567fefc7
fernando-henrique <fernandohsferreira@gmail.com>
2024-01-17 20:43:18 -0400

    atualizando arquivos

---
+++ back/src/scripts/services/repositories.js
@@ -0,0 +1,7 @@
+import { baseUrl, repositoriesQuantity } from "/src/scripts/variables.js"
+
+async function getRepos(userName){
+    const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesQuantity}`)
+    return await response.json()
+}
+export{getRepos}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,7 @@
 import { getUser } from "./services/users.js"
-import { getRepos } from "./services/repositories.js"
+import { getRepos } from "./services//repositories.js"
 import { user } from "./objects/user.js"
-import { screen } from "./objects/screen.js"
+import { screen } from "./objects//screen.js"
 
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
