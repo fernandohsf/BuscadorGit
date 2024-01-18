289764e98825804f2d0a4f648c04f3547439b67f
Fernando Henrique da Silva Ferreira <fernandohsferreira@gmail.com>
2024-01-18 08:33:42 -0400

    Add files via upload

---
+++ src/scripts/services/repositories.js
@@ -0,0 +1,7 @@
+import { baseUrl, repositoriesQuantity } from "../variables.js"
+
+async function getRepos(userName){
+    const response = await fetch(`${baseUrl}/${userName}/repos?per_page${repositoriesQuantity}`)
+    return await response.json()
+}
+export {getRepos}
